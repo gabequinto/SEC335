9fc9513d23e305f0973a4b79ef4afe90b51423da
gabequinto <78443183+gabequinto@users.noreply.github.com>
2022-02-07 19:17:19 -0500

    Update dns-resolver.sh

--- week3/dns-resolver.sh
+++ week3/dns-resolver.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+host=$1
+server=$2
+
+
+
+echo "dns resolution for 10.0.5"
+for host in `seq 1 254`; do
+(nslookup $1.$host $2) |  awk -F "= " '/name/{print $0}' 
+                done 
 
